5c21e45ea4d6d264e1c679d7e0c8275259baa4f6
Sergio Lucero Corchado <55106535+SergioCyber@users.noreply.github.com>
2022-01-19 14:15:36 -0500

    Add files via upload
    binario.py

---
+++ binario.py
@@ -0,0 +1,25 @@
+def esBinario (strbinario):
+    try:
+        for dato in range(0, len(strbinario)):
+            if strbinario[dato]!='1' and strbinario[dato]!='0':
+                return False
+        return True
+    except TypeError:
+        return False
+
+def ext (bin):
+    try:
+        dec=0
+        for dato in range(0, len(bin)):
+            if bin[dato]=='1':
+                dec=dec+pow(2, len(bin)-dato-1)
+        return dec
+    except TypeError:
+        return False
+
+pantalla=str(input("Escribe un número que sea binario:  "))
+
+if esBinario(pantalla):
+    print(f"{pantalla} es binario " + str(ext(pantalla)))
+else:
+    print(f"{pantalla} ¡No es un número binario!")
