6a2e82836d407dcfcb8d442c6839685c0b848849
Sergio Lucero Corchado <55106535+SergioCyber@users.noreply.github.com>
2022-01-19 20:16:39 +0100

    Update binario.py

--- binario.py
+++ binario.py
@@ -1,4 +1,6 @@
 def esBinario (strbinario):
+    
+    
     try:
         for dato in range(0, len(strbinario)):
             if strbinario[dato]!='1' and strbinario[dato]!='0':
@@ -7,6 +9,7 @@ def esBinario (strbinario):
     except TypeError:
         return False
 
+    
 def ext (bin):
     try:
         dec=0
@@ -16,8 +19,10 @@ def ext (bin):
         return dec
     except TypeError:
         return False
+    
 
 pantalla=str(input("Escribe un número que sea binario:  "))
+
 
 if esBinario(pantalla):
     print(f"{pantalla} es binario " + str(ext(pantalla)))
